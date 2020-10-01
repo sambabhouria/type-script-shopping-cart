d59e121c7fdff1881dc5542db6e8f95e4d3df33d
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-10-01 18:50:04 +0200

    products component added.

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -2,17 +2,73 @@ import React, { Fragment, useContext, useEffect, useState } from 'react';
 import Helmet from 'react-helmet';
 import { RouteComponentProps } from 'react-router-dom';
 import { Header, Segment } from 'semantic-ui-react';
-
+import CartContext from '../cart/context/CartContext';
+import CartContextManager from '../cart/context/CartContextManager';
+import GridProucts from './components/GridProucts';
+import ModalProduct from './components/ModalProduct';
+import ProductCard from './components/ProductCard';
+import Product from './Product';
+import ProductsService from './ProductsService';
 
 const Products: React.FC<RouteComponentProps> = () => {
 
-    return (
-        <Fragment>
-          <Helmet>
-            <title>Produtos</title>
-          </Helmet>
-          </Fragment>
-    )
- }
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoadding, setIsLoadding] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [productToShow, setProductToShow] = useState<Product>();
+  const { addItem, hasInTheCart } = useContext<CartContextManager>(CartContext);
+
+  useEffect(() => {
+    setIsLoadding(true);
+    ProductsService.getAll()
+      .then(response => {
+        setProducts(response);
+        setIsLoadding(false);
+      })
+      .catch(() => setIsLoadding(false));
+  }, []);
+
+  function closeModal() {
+    setShowModal(false);
+    setProductToShow(undefined);
+  }
+
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Produtos</title>
+      </Helmet>
+      {productToShow && (
+        <ModalProduct
+          isInTheCart={hasInTheCart(productToShow)}
+          onClose={closeModal}
+          product={productToShow}
+          onAdd={() => addItem(productToShow)}
+          open={showModal}
+        />
+      )}
+      <Segment placeholder>
+        <Header textAlign="center" as="h1">
+          Produtos
+        </Header>
+      </Segment>
+      <GridProucts
+        products={products}
+        isLoadding={isLoadding}
+        renderProduct={product => (
+          <ProductCard
+            onViewCard={() => {
+              setProductToShow(product);
+              setShowModal(true);
+            }}
+            onAddtoCart={addItem}
+            product={product}
+            hasInTheCart={hasInTheCart(product)}
+          />
+        )}
+      />
+    </Fragment>
+  );
+};
 
 export default Products;
